227fb381a106053e0e3e65df7fd0e02f9bfd0ffe
hanajjang25 <hanajjang25@naver.com>
2024-04-30 09:05:43 +0900

    project

--- lib/nonMember.dart
+++ lib/nonMember.dart
@@ -3,10 +3,191 @@ import 'package:flutter/material.dart';
 class nonMember extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('비회원', style: TextStyle(fontSize: 24.0)),
-      ),
+    return Column(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          clipBehavior: Clip.antiAlias,
+          decoration: BoxDecoration(color: Colors.white),
+          child: Stack(
+            children: [
+              Positioned(
+                left: (MediaQuery.of(context).size.width - 300) / 2,
+                top: 248,
+                child: Container(
+                  width: 300,
+                  height: 60,
+                  decoration: ShapeDecoration(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 3, color: Color(0xFFD9D9D9)),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width + 210) / 2,
+                top: 268,
+                child: Container(
+                  width: 18,
+                  height: 20,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: 14,
+                          height: 14,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shape: OvalBorder(side: BorderSide(width: 1)),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 11,
+                        top: 12,
+                        child: Transform(
+                          transform: Matrix4.identity()
+                            ..translate(0.0, 0.0)
+                            ..rotateZ(0.85),
+                          child: Container(
+                            width: 10.63,
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  width: 1,
+                                  strokeAlign: BorderSide.strokeAlignCenter,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width + 310) / 2,
+                top: 99,
+                child: Container(
+                  width: 20,
+                  height: 11,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: Container(
+                          width: 20,
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        top: 6,
+                        child: Container(
+                          width: 20,
+                          decoration: ShapeDecoration(
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                width: 1,
+                                strokeAlign: BorderSide.strokeAlignCenter,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width + 5) / 2,
+                top: 90,
+                child: Container(
+                  width: 36.43,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage("https://via.placeholder.com/36x30"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width + 75) / 2,
+                top: 92,
+                child: SizedBox(
+                  width: 97,
+                  height: 28,
+                  child: Text(
+                    '로그인하세요',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w700,
+                      height: 0,
+                      letterSpacing: -0.30,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width + 295) / 2,
+                top: 113,
+                child: Transform(
+                  transform: Matrix4.identity()
+                    ..translate(0.0, 0.0)
+                    ..rotateZ(-3.14),
+                  child: Container(
+                    width: 15,
+                    height: 15,
+                    decoration: ShapeDecoration(
+                      color: Color(0xFFD9D9D9),
+                      shape: StarBorder.polygon(sides: 3),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: (MediaQuery.of(context).size.width - 90) / 2,
+                top: 505,
+                child: SizedBox(
+                  width: 90,
+                  height: 29,
+                  child: Text(
+                    '대기순번',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontFamily: 'Noto Sans KR',
+                      fontWeight: FontWeight.w900,
+                      height: 0,
+                      letterSpacing: -0.30,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
