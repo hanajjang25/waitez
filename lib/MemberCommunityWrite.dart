4cd035e7a4989172f17d68bb41fae061caefc3be
eunjung-365 <83387104+eunjung-365@users.noreply.github.com>
2024-08-23 14:45:57 +0900

    Update MemberCommunityWrite.dart

--- lib/MemberCommunityWrite.dart
+++ lib/MemberCommunityWrite.dart
@@ -8,19 +8,21 @@ class WritePostPage extends StatefulWidget {
 }
 
 class _WritePostPageState extends State<WritePostPage> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _contentController = TextEditingController();
-  final User? currentUser = FirebaseAuth.instance.currentUser;
-  bool _isTitleEmpty = false;
-  bool _isContentEmpty = false;
-  String? _titleError;
-  String? _contentError;
+  final TextEditingController _titleController = TextEditingController(); // 제목 입력 필드 컨트롤러
+  final TextEditingController _contentController = TextEditingController(); // 내용 입력 필드 컨트롤러
+  final User? currentUser = FirebaseAuth.instance.currentUser; // 현재 로그인된 사용자 정보
+  bool _isTitleEmpty = false; // 제목이 비어있는지 확인하는 플래그
+  bool _isContentEmpty = false; // 내용이 비어있는지 확인하는 플래그
+  String? _titleError; // 제목 입력 오류 메시지
+  String? _contentError; // 내용 입력 오류 메시지
 
+  // 입력된 텍스트가 유효한 한글인지 확인하는 함수
   bool _isValidKorean(String value) {
     final koreanRegex = RegExp(r'^[가-힣\s]+$');
     return koreanRegex.hasMatch(value);
   }
 
+  // 게시물을 제출하는 함수
   Future<void> submitPost() async {
     final title = _titleController.text;
     final content = _contentController.text;
@@ -31,6 +33,7 @@ class _WritePostPageState extends State<WritePostPage> {
       _titleError = null;
       _contentError = null;
 
+      // 제목 입력 유효성 검사
       if (title.isEmpty) {
         _titleError = '제목을 입력하세요';
       } else if (!_isValidKorean(title)) {
@@ -39,6 +42,7 @@ class _WritePostPageState extends State<WritePostPage> {
         _titleError = '10자 이하로 입력해주세요';
       }
 
+      // 내용 입력 유효성 검사
       if (content.isEmpty) {
         _contentError = '내용을 입력하세요';
       } else if (content.length < 5) {
@@ -50,6 +54,7 @@ class _WritePostPageState extends State<WritePostPage> {
       }
     });
 
+    // 제목과 내용이 유효한 경우 Firestore에 게시물 저장
     if (_titleError == null && _contentError == null) {
       if (currentUser != null) {
         try {
@@ -59,10 +64,10 @@ class _WritePostPageState extends State<WritePostPage> {
               .doc(currentUser!.uid)
               .get();
 
+          // 닉네임을 가져왔을 경우 Firestore에 게시물 저장
           if (userDoc.exists && userDoc.data() != null) {
             final nickname = userDoc.get('nickname');
 
-            // Firestore에 게시물 저장
             await FirebaseFirestore.instance.collection('community').add({
               'title': title,
               'content': content,
@@ -72,6 +77,7 @@ class _WritePostPageState extends State<WritePostPage> {
               'timestamp': DateTime.now(),
             });
 
+            // 게시물 저장 후 이전 페이지로 돌아감
             Navigator.pop(context);
           } else {
             _showErrorDialog('글쓰기 실패하였습니다');
@@ -85,6 +91,7 @@ class _WritePostPageState extends State<WritePostPage> {
     }
   }
 
+  // 오류 다이얼로그를 표시하는 함수
   void _showErrorDialog(String message) {
     showDialog(
       context: context,
@@ -107,8 +114,8 @@ class _WritePostPageState extends State<WritePostPage> {
 
   @override
   void dispose() {
-    _titleController.dispose();
-    _contentController.dispose();
+    _titleController.dispose(); // 제목 입력 필드 컨트롤러 해제
+    _contentController.dispose(); // 내용 입력 필드 컨트롤러 해제
     super.dispose();
   }
 
@@ -116,7 +123,7 @@ class _WritePostPageState extends State<WritePostPage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        return true; // Allow back navigation without any additional actions
+        return true; // 추가 작업 없이 뒤로 가기를 허용
       },
       child: Scaffold(
         appBar: AppBar(
@@ -148,32 +155,32 @@ class _WritePostPageState extends State<WritePostPage> {
                     child: Divider(color: Colors.black, thickness: 2.0)),
                 SizedBox(height: 50),
                 TextField(
-                  controller: _titleController,
+                  controller: _titleController, // 제목 입력 필드
                   decoration: InputDecoration(
                     labelText: '제목',
-                    errorText: _titleError,
+                    errorText: _titleError, // 제목 오류 메시지 표시
                   ),
                 ),
                 SizedBox(height: 20),
                 TextField(
-                  controller: _contentController,
+                  controller: _contentController, // 내용 입력 필드
                   decoration: InputDecoration(
                     labelText: '내용',
-                    errorText: _contentError,
+                    errorText: _contentError, // 내용 오류 메시지 표시
                   ),
                   maxLines: 5,
                 ),
                 SizedBox(height: 50),
                 ElevatedButton(
-                  onPressed: submitPost,
+                  onPressed: submitPost, // 게시물 제출 버튼 클릭 시
                   child: Text('등록'),
                   style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
                     backgroundColor: !_isTitleEmpty && !_isContentEmpty
-                        ? Colors.lightBlueAccent
-                        : Colors.red,
+                        ? Colors.lightBlueAccent // 제목과 내용이 모두 입력된 경우
+                        : Colors.red, // 제목이나 내용이 비어있는 경우
                   ),
                 ),
               ],
