5202bbe3d99442bc9900fb1da052be780d21cf39
hanajjang25 <93505567+hanajjang25@users.noreply.github.com>
2024-08-24 00:02:58 +0900

    Update NonMemberBottom.dart

--- lib/NonMemberBottom.dart
+++ lib/NonMemberBottom.dart
@@ -9,23 +9,22 @@ class nonMemberBottom extends StatelessWidget {
   Widget build(BuildContext context) {
     // 이 StatelessWidget의 UI를 빌드하는 메소드.
     return BottomNavigationBar(
-      selectedItemColor: Colors.black, // 선택된 아이템의 아이콘 및 텍스트 색상을 검정으로 설정.
-      unselectedItemColor: Colors.black, // 선택되지 않은 아이템의 아이콘 및 텍스트 색상을 검정으로 설정.
-      type: BottomNavigationBarType.fixed, // 네비게이션 바 타입을 고정형으로 설정하여 아이템의 개수가 많아도 동일한 간격을 유지.
-      selectedFontSize: 13, // 선택된 아이템의 텍스트 크기를 13으로 설정.
-      unselectedFontSize: 13, // 선택되지 않은 아이템의 텍스트 크기를 13으로 설정. 선택된 아이템과 비선택된 아이템의 텍스트 크기를 동일하게 유지.
+      selectedItemColor: Colors.black,                                 // 선택된 아이템의 아이콘 및 텍스트 색상을 검정으로 설정.
+      unselectedItemColor: Colors.black,                               // 선택되지 않은 아이템의 아이콘 및 텍스트 색상을 검정으로 설정.
+      type: BottomNavigationBarType.fixed,                             // 네비게이션 바 타입을 고정형으로 설정하여 아이템의 개수가 많아도 동일한 간격을 유지.
+      selectedFontSize: 13,                                            // 선택된 아이템의 텍스트 크기를 13으로 설정.
+      unselectedFontSize: 13,                                          // 선택되지 않은 아이템의 텍스트 크기를 13으로 설정. 선택된 아이템과 비선택된 아이템의 텍스트 크기를 동일하게 유지.
       
       // 아이템이 탭될 때 호출되는 콜백 함수. 인덱스를 통해 어떤 아이템이 선택되었는지 확인.
       onTap: (int index) {
         switch (index) {
-          case 0: // 첫 번째 아이템 (Home) 이 선택된 경우.
-            Navigator.pushNamed(context, '/nonMemberHome'); // '/nonMemberHome' 라우트로 이동.
+          case 0:                                                      // 첫 번째 아이템 (Home) 이 선택된 경우.
+            Navigator.pushNamed(context, '/nonMemberHome');            // '/nonMemberHome' 경로로 이동.
             break;
-          case 1: // 두 번째 아이템 (Reservation) 이 선택된 경우.
-            Navigator.pushNamed(context, '/search'); // '/search' 라우트로 이동.
+          case 1:                                                      // 두 번째 아이템 (Reservation) 이 선택된 경우.
+            Navigator.pushNamed(context, '/search');                   // '/search' 경로로 이동.
             break;
           default:
-            // 추가적인 케이스를 처리할 수 있는 위치. 현재는 아무런 작업도 수행하지 않음.
         }
       },
       
